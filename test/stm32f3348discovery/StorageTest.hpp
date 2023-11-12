9d649f4d69aa679df29cef30e0a849733e3df7bf
Jochen Wilhelmy <jochen.wilhelmy@gmail.com>
2023-11-12 19:12:03 +0100

    Add STM32F3

---
+++ test/stm32f3348discovery/StorageTest.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <coco/Storage_Buffer.hpp>
+#include <coco/platform/Loop_TIM2.hpp>
+#include <coco/platform/Flash_FLASH.hpp>
+#include <coco/board/config.hpp>
+
+
+using namespace coco;
+
+Storage_Buffer::Info storageInfo{
+	Flash_FLASH::PAGE_SIZE,
+	Flash_FLASH::BLOCK_SIZE,
+	FLASH_ADDRESS + FLASH_SIZE - 8192 * 2, // address
+	8192, // sector size
+	2, // sector count
+	Storage_Buffer::Type::FLASH_4N
+};
+
+
+// drivers for FlashTest
+struct Drivers {
+	Loop_TIM2 loop{CLOCK};
+	Flash_FLASH::Buffer<256> buffer;
+};
